1a6eefb0123b1777abb1aea7785fa09dc666345b
unknown <haitham8213@gmail.com>
2025-11-09 18:25:30 +0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/IndulgeWebsite/', // <- replace with your GitHub repo name
+  base: '/IndulgeWebsite/',
   plugins: [react()],
 });
